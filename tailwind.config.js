7296c98a99ffe4e0bc0b8bc1cdf4cf4aaa07f2e6
Mauriz66 <76722408+Mauriz66@users.noreply.github.com>
2025-03-08 01:24:17 -0300

    Adicionando Tailwind CSS e melhorando estilos

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0088FE',
+        secondary: '#00C49F',
+        accent: '#FFBB28',
+        warning: '#FF8042',
+        purple: '#8884d8',
+        green: '#82ca9d'
+      }
+    },
+  },
+  plugins: [],
+} 
